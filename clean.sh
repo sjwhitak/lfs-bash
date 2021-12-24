7dddf7e0a480a990f168a04b3ad0329c709b136f
Steven Whitaker <sjwhitak@mtu.edu>
2021-12-23 21:34:50 -0500

    Push into a single  and  script

--- clean.sh
+++ clean.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
+
 export LFS=/mnt/lfs
-bash lfs-cleanup.sh
+
+echo "WARNING WARNING WARNING"
+read -p "This will delete everything. Proceed? [y/N] " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+	echo "Nothing has been changed."
+	exit 1
+fi
+
+if [[ -d $LFS ]]
+then
+	[[ -d $LFS/boot ]] && echo "Deleting $LFS/boot"; rm -rf $LFS/boot/*; umount $LFS/boot
+	[[ -d $LFS/home ]] && echo "Deleting $LFS/home"; rm -rf $LFS/home/*; umount $LFS/home
+	
+	echo "Deleting $LFS"; rm -rf $LFS/*; umount $LFS
+	rmdir $LFS
+else
+	echo "$LFS is already cleaned."
+fi
+
+echo "Deleting lfs user and group"
+if id -u "lfs" &>/dev/null; then deluser --remove-home lfs; fi
+
+
--- make.sh
+++ make.sh
@@ -1,18 +1,73 @@
 #!/bin/bash
+# Mounts the proper drives set up where
+# /dev/sda2 = /boot
+# /dev/sda3 = /
+# /dev/sda4 = /home
+#
+# and
+# $LFS = /mnt/lfs
+#
+# Note there is NO swap partition since I'm on MBR/DOS, there 
+# are only 4 partitions available!
+# TODO: You need to remember to add a swapfile.
+
+# Globals
 export LFS=/mnt/lfs
 
-echo "Creating user"
-bash ./lfs-user.sh
+### Set up partitions -----------------------------
+echo "Setting up partitions"
 
-echo "Mounting folders"
-bash ./lfs-mount.sh
+[[ ! -d "$LFS" ]] && mkdir $LFS
+[[ ! $(findmnt -M "$LFS") ]] && mount /dev/sda3 $LFS
 
-echo "Populating folders"
-bash ./lfs-folder.sh
+[[ ! -d "$LFS/home" ]] && mkdir $LFS/home
+[[ ! $(findmnt -M "$LFS/home") ]] && mount /dev/sda4 $LFS/home
 
-# Copy source tarball into $LFS/sources
+[[ ! -d "$LFS/boot" ]] && mkdir $LFS/boot
+[[ ! $(findmnt -M "$LFS/boot") ]] && mount /dev/sda2 $LFS/boot
+
+### Set up partitions -----------------------------
+### File structure --------------------------------
+echo "Setting up file structure"
+
+source_folders=("etc" "var" "usr/bin" \
+	"usr/lib" "usr/sbin" "tools" "sources")
+for i in "${source_folders[@]}"
+do
+	[[ ! -d $LFS/$i ]] && mkdir -p $LFS/$i
+done
+
+for i in bin lib sbin
+do
+	[[ ! -L $LFS/$i ]] && ln -s usr/$i $LFS/$i
+done
+
+# Make lib64 if 64-bit
+if [[ "$(uname -m)" == "x86_64" ]]
+then
+	[[ ! -d $LFS/lib64 ]] && mkdir $LFS/lib64
+fi
+
+### File structure --------------------------------
+### Set up user -----------------------------------
+echo "Setting up lfs user"
+
+# Add LFS group and user
+[[ ! $(getent group lfs) ]] && groupadd lfs
+if ! id -u "lfs" &>/dev/null; then
+	useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+fi
+
+# Make $LFS under lfs
+chown -R lfs:lfs $LFS
+
+# Set up the user
+cp lfs.bashrc /home/lfs/.bashrc
+cp lfs.bashprofile /home/lfs/.bash_profile
 
 
-# Go into the lfs environment
-su - lfs
+# Ubuntu has a default .bashrc
+[ ! -e /etc/bash.bashrc ] || mv /etc/bash.bashrc /etc/bash.bashrc.NOUSE
+
+### Set up user -----------------------------------
 
